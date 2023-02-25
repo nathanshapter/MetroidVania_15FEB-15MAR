8378c7da2eccc4257a8886d1cb769589eb8e4204
Nathan Shapter <shapternathan@protonmail.com>
2023-02-25 13:26:25 -0500

    spikes fall in intervals, working on difficulty stages

--- Assets/Scripts/Enemy/BossFights/SpikeSpawner.cs
+++ Assets/Scripts/Enemy/BossFights/SpikeSpawner.cs
@@ -1,8 +1,55 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpikeSpawner : MonoBehaviour
 {
-   
+    [SerializeField] Transform[] spawnPositions;
+
+    [SerializeField] GameObject fallingSpike;
+
+    int spikesSpawned;
+  [SerializeField]  int minSpikesToSpawn;
+   [SerializeField] int maxSpikesToSpawn;
+
+
+    float timeSinceLastDrop;
+    private void Start()
+    {
+        SpawnSpikes();
+    }
+
+    private void SpawnSpikes()
+    {
+        foreach (Transform i in spawnPositions)
+        {
+            if (Random.Range(0, 10) > 4)
+            {
+                
+                Instantiate(fallingSpike, i);
+                spikesSpawned++;
+                if(spikesSpawned>= maxSpikesToSpawn) { spikesSpawned = 0; break; }
+            }
+
+        }
+    }
+
+
+    private void Update()
+    {
+        timeSinceLastDrop += Time.deltaTime;
+        if(timeSinceLastDrop > 5)
+        {
+            timeSinceLastDrop= 0;
+            SpawnSpikes();
+        }
+    }
+    public IEnumerator DropSpikes()
+    {
+                
+    //        SpawnSpikes();
+
+        yield return null;
+    }
 }
