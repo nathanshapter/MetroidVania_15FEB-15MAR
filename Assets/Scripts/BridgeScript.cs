616cccb11b4135dd99227dde53c8f71f9376f32e
Nathan Shapter <shapternathan@protonmail.com>
2023-03-02 01:27:34 -0500

    working on bridges

--- Assets/Scripts/BridgeScript.cs
+++ Assets/Scripts/BridgeScript.cs
@@ -5,10 +5,31 @@ using UnityEngine;
 public class BridgeScript : MonoBehaviour
 {
   [SerializeField]  HingeJoint2D[] hingeJoints;
-
+   [SerializeField] GameObject[] hinge; // has to be entered manually
+    [SerializeField] bool disableHinge;
 
     private void Start()
     {
         hingeJoints = GetComponentsInChildren<HingeJoint2D>();
+      
+    }
+
+    private void Update()
+    {
+        if(disableHinge)
+        {
+            StartCoroutine(DisableAndDestroy());
+        }
+    }
+    private IEnumerator DisableAndDestroy()
+    {
+        foreach (HingeJoint2D joint in hingeJoints)
+        {
+            joint.enabled = false;
+            yield return new WaitForSeconds(Random.Range(0.2f,0.5f));
+           
+            
+        }
+      
     }
 }
