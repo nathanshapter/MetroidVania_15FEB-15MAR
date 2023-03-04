c02e26b59bcbbc2a6d520f62b1363fedbe04845d
Nathan Shapter <shapternathan@protonmail.com>
2023-03-04 13:28:22 -0500

    working on 1j & 1n, and enemy shooting fireballs

--- Assets/Scripts/Enemy/EnemyStaticShooter.cs
+++ Assets/Scripts/Enemy/EnemyStaticShooter.cs
@@ -8,25 +8,75 @@ public class EnemyStaticShooter : MonoBehaviour
 
     [SerializeField] float aggroRange, aggroAwakeRange;
     bool awake = false;
+    [SerializeField] float rotationOffset;
 
+
+    float distanceBetweenPlayer;
+    Transform target; // player
+
+
+    [Header("Ball Properties")]
+    [SerializeField]  int amountOfBallsSpawned, amountOfBallsToSpawn;
+    [SerializeField] float timeBetweenFireballSpawn;
+    [SerializeField] GameObject fireball;
+    [SerializeField] Transform fireballSpawn;
+   [SerializeField] float timeSinceLastWave;
+    [SerializeField] float timeBetweenWaves = 5f;
+    bool waveInProgress = false;
+    private void Awake()
+    {
+        target = FindObjectOfType<Health>().gameObject.transform;
+    }
+    private void Start()
+    {
+        StartCoroutine(SpawnFireBalls());
+    }
     private void Update()
-    {       
+    {
+        
+        distanceBetweenPlayer = Vector3.Distance(target.transform.position, transform.position);
+        print(distanceBetweenPlayer);
+        LookAtPlayer();
+        if (!waveInProgress)
+        {
+            timeSinceLastWave += Time.deltaTime;
+        }
+        
+        if(timeSinceLastWave >= timeBetweenWaves)
+        {
+            if(!waveInProgress)
+            {
+                StartCoroutine(SpawnFireBalls());
+            }
+            
 
-        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
-      // print(distanceToPlayer);
-        if (distanceToPlayer < aggroRange)
-        {
-            awake = true;
-            transform.right = player.position - transform.position;
         }
-        else if (distanceToPlayer < aggroAwakeRange && awake == true)
+    }
+    IEnumerator SpawnFireBalls()
+    {
+        waveInProgress= true;
+       
+        if (amountOfBallsSpawned < amountOfBallsToSpawn)
         {
-            transform.right = player.position - transform.position;
+            for (amountOfBallsSpawned = 0; amountOfBallsSpawned < amountOfBallsToSpawn; amountOfBallsSpawned++)
+            {              
+               
+                
+                    yield return new WaitForSeconds(timeBetweenFireballSpawn);
+                    Instantiate(fireball, fireballSpawn.position, fireballSpawn.transform.rotation);
+
+                
+
+            }
+            timeSinceLastWave = 0;
+            waveInProgress= false;
+            amountOfBallsSpawned = 0;
         }
-        else if (distanceToPlayer > aggroAwakeRange)
-        {
-            awake = false;
-            // return to original spot
-        }
+    }
+    public void LookAtPlayer()
+    {
+        var dir = target.position - this.transform.position;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg + rotationOffset;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 }
---
+++ Assets/Scripts/Enemy/RegularFireballScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegularFireballScript : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    [SerializeField] Health target; // to target player
+    Rigidbody2D rb;
+    Vector2 moveDirection;
+    [SerializeField] private AudioClip fireBall;
+
+
+    private void Start()
+    {
+        // add sound to fireball
+        target = FindObjectOfType<Health>();
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
+        moveDirection = (new Vector3(target.transform.position.x , target.transform.position.y, 0) - transform.position).normalized *moveSpeed;
+        rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+}
