80c7370886f73e9b3a251b9e158004a2774ac2de
Nathan Shapter <shapternathan@protonmail.com>
2023-03-13 22:20:15 -0400

    still working on above

---
+++ Assets/Scripts/Player Scripts/PlayerAnimationEvents.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationEvents : MonoBehaviour
+{
+    [Header("Effects")]
+    public GameObject m_RunStopDust;
+    public GameObject m_JumpDust;
+    public GameObject m_LandingDust;
+    public GameObject m_DodgeDust;
+    public GameObject m_WallSlideDust;
+    public GameObject m_WallJumpDust;
+    public GameObject m_AirSlamDust;
+    public GameObject m_ParryEffect;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
