f2e3c0f63709e3ddfabc8bafbe418ebd88cb98c0
Nathan Shapter <shapternathan@protonmail.com>
2023-03-25 16:56:45 -0400

    chronological platforms working perfectly

--- Assets/Scripts/Platforms/ChronologicalPlatform.cs
+++ Assets/Scripts/Platforms/ChronologicalPlatform.cs
@@ -4,11 +4,48 @@ using UnityEngine;
 
 public class ChronologicalPlatform : MonoBehaviour
 {
+    ChronologicalPlatformManager manager;
+    bool hasSpawnedNextPlatform = false;
+   [SerializeField] bool isFirstPlatform = false;
+    bool isTimed;
+     float timeUntilCrumble;
+
+    private void Start()
+    {
+        manager = GetComponentInParent<ChronologicalPlatformManager>();
+        isTimed = GetComponentInParent<ChronologicalPlatformManager>().isTimed;
+        timeUntilCrumble = GetComponentInParent<ChronologicalPlatformManager>().timeUntilCrumble;
+    }
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") && !hasSpawnedNextPlatform)
         {
-            print("he;l");
+            manager.ProcessPlatforms();
+            
+            hasSpawnedNextPlatform = true;
+            if(isTimed && !isFirstPlatform)
+            {
+                StartCoroutine(DisableTimer());
+            }
+           
         }
     }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && !isFirstPlatform)
+        {
+            Disable();
+        }
+    }
+    private IEnumerator DisableTimer()
+    {
+        yield return new WaitForSeconds(timeUntilCrumble);
+        Disable();
+    }
+    private void Disable()
+    {
+        GetComponentInChildren<Renderer>().enabled = false;
+        GetComponent<BoxCollider2D>().enabled = false;
+    }
+
 }
--- Assets/Scripts/Platforms/ChronologicalPlatformManager.cs
+++ Assets/Scripts/Platforms/ChronologicalPlatformManager.cs
@@ -6,8 +6,11 @@ public class ChronologicalPlatformManager : MonoBehaviour
 {
     [SerializeField]
     GameObject[] platform;
-   
-  
+  [SerializeField]  int previousPlatform = 0;
+    [SerializeField] int totalPlatforms;
+    public bool isTimed;
+  public  float timeUntilCrumble;
+
 
     private void Start()
     {
@@ -16,7 +19,45 @@ public class ChronologicalPlatformManager : MonoBehaviour
             item.gameObject.SetActive(false);
         }
         platform[0].gameObject.SetActive(true);
+        totalPlatforms = platform.Length;
     }
 
+    private void Update()
+    {
+         Renable();
+    }
+    public void ProcessPlatforms()
+    {
 
+
+
+
+        if (previousPlatform +1 >= totalPlatforms) { previousPlatform = 0; previousPlatform--; }
+
+
+        platform[previousPlatform + 1].gameObject.SetActive(true);
+
+     
+
+        previousPlatform++;
+     
+
+      
+    }
+    void Renable()
+    {
+        if(Input.GetKeyUp(KeyCode.Escape))
+        {
+            foreach (var item in platform)
+            {
+                item.GetComponentInChildren<Renderer>().enabled = true;
+                item.GetComponent<BoxCollider2D>().enabled = true;
+            }
+        }
+        
+      
+
+
+
+    }
 }
