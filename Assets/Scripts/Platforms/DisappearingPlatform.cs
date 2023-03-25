ef428b1007f7e374f27045b6e64eff0515b5d429
Nathan Shapter <shapternathan@protonmail.com>
2023-03-25 10:46:55 -0400

    platform correctly dis and reappers

--- Assets/Scripts/Platforms/DisappearingPlatform.cs
+++ Assets/Scripts/Platforms/DisappearingPlatform.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class DisappearingPlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    PlayerMovement playerMovement;
+    [SerializeField] int timeUntilDestroy =3;
+    [SerializeField] int timeUntilRespawn = 3;
+    BoxCollider2D boxCollider2D;
+    SpriteRenderer sprite;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        playerMovement = FindObjectOfType<PlayerMovement>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+
     }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(PlatformEnableDisable());
+        }
+    }
+    IEnumerator PlatformEnableDisable()
+    {
+       
+        yield return new WaitForSeconds(timeUntilDestroy);
+        playerMovement.transform.SetParent(null);
+        sprite.enabled = false; // instead of being false, future update will have it play an animation, and turn into nothing
+        boxCollider2D.enabled = false;
+       
+        yield return new WaitForSeconds(timeUntilRespawn);
+        this.gameObject.SetActive(true);
+        sprite.enabled = true;
+        boxCollider2D.enabled = true;
+       
+    }
+   
 }
