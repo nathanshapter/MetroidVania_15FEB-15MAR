468c5fd85f2e468aec7c72e4b68da575fa027a2a
Nathan Shapter <shapternathan@protonmail.com>
2023-03-27 18:55:13 -0400

    electrcity knockback working well, decide in inspector to knock left or right

--- Assets/Scripts/Traps/ElectricityStatic.cs
+++ Assets/Scripts/Traps/ElectricityStatic.cs
@@ -6,11 +6,22 @@ public class ElectricityStatic : MonoBehaviour
 {
   [SerializeField]  int elecDmg;
     [SerializeField] int knockback = 25;
+
+   [SerializeField] bool knockToRight;
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            other.gameObject.GetComponent<Health>().TakeDamage(elecDmg, knockback, knockback);
+           
+                other.gameObject.GetComponent<Health>().TakeDamage(elecDmg, RightOrLeft(), knockback);
+           
+           
+            
         }
+    }
+    int RightOrLeft()
+    {
+        if (knockToRight) { return elecDmg; }
+        else { return -elecDmg; }
     }
 }
