2007a024857a755177af1244f8b9fd5a0e67d14f
Nathan Shapter <shapternathan@protonmail.com>
2023-05-10 10:51:00 -0400

    reverted to commit, save slots too buggy, wil ltry again at a later date

--- Assets/Scripts/SaveData/SaveDataManager.cs
+++ Assets/Scripts/SaveData/SaveDataManager.cs
@@ -56,7 +56,7 @@ public class SaveDataManager : MonoBehaviour
     public void NewGame() 
     {
         gameData = new GameData();
-        print(" hello5");
+      
     }
 
     public void LoadGame()
