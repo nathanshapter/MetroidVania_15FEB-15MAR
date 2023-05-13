07594219dc6cd15323aabfd7515537d4d71b001c
Nathan Shapter <shapternathan@protonmail.com>
2023-05-13 19:15:30 -0400

    added varying timer to turn ice lights on/off off sourballs the sourpuss suggestion

--- Assets/Scripts/Lights/LightFlicker.cs
+++ Assets/Scripts/Lights/LightFlicker.cs
@@ -11,15 +11,32 @@ public class LightFlicker : MonoBehaviour
     [SerializeField] bool turnOnProximity = false;
     PlayerMovement player;
     [SerializeField] int turnOnDistance = 5;
-
-
+    [SerializeField] bool varyLights;
+    [SerializeField] bool turnOffRandomly;
+    [SerializeField]
+    [Range(0f, 100f)] float chanceToTurnOffPercentage;
+    float periodIncrease;
+    [SerializeField] float timeToStayOff = 5;
+    [SerializeField] int timeBetweenTicks = 1;
+    int timeBetweenTicksVariance = 0;
     private void Start()
     {
+        timeBetweenTicksVariance = Random.Range(-5, 5);
         player= FindObjectOfType<PlayerMovement>();
+        if(varyLights)
+        {
+            periodIncrease = Random.Range(0, 5);
+        }
+        else { periodIncrease = 0; }
+        
     }
-
+ [SerializeField]   bool lightOn = true;
     private void Update()
     {
+        if (!lightOn) { return; }
+        float numberVariance = Random.Range(0, 0.2f);
+
+
 
         if (turnOnProximity)
         {
@@ -32,15 +49,35 @@ public class LightFlicker : MonoBehaviour
             return;
         }
 
-        float cycles = Time.time / period;
+        float cycles = Time.time / (period  + periodIncrease);
 
         const float tau = Mathf.PI * 2;
         float rawSinWave = Mathf.Sin(cycles * tau +1);
 
-        iceLight.intensity = (rawSinWave + 1) +addedIntensity;
+        iceLight.intensity = (rawSinWave + 1) +addedIntensity * (numberVariance) +1;
 
 
-       
+       if(turnOffRandomly && !isRunningCoroutine)
+        {
+            StartCoroutine(TurnOffChance());
+        }
       
+    }
+    bool isRunningCoroutine;
+   float percentageToTurnOff;
+    IEnumerator TurnOffChance()
+    {
+        percentageToTurnOff = Random.Range(0, 100);
+        print(percentageToTurnOff);
+        isRunningCoroutine= true;
+        yield return new WaitForSeconds(timeBetweenTicks + timeBetweenTicksVariance);
+        if (percentageToTurnOff > chanceToTurnOffPercentage)
+        {
+            lightOn= false;
+            iceLight.intensity = 0;
+            yield return new WaitForSeconds(timeToStayOff);
+            lightOn= true;
+        }
+        isRunningCoroutine= false;
     }
 }
